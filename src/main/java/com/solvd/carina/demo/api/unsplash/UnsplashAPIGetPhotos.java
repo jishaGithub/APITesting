0eed6c50d2209ed3d7d78d7d778da6c3de37f158
JishaG <jishar2023@gmail.com>
2023-07-31 14:31:22 -0400

    changes to resources and other test files

--- src/main/java/com/solvd/carina/demo/api/unsplash/UnsplashAPIGetPhotos.java
+++ src/main/java/com/solvd/carina/demo/api/unsplash/UnsplashAPIGetPhotos.java
@@ -1,23 +1,16 @@
 package com.solvd.carina.demo.api.unsplash;
 
-import com.zebrunner.carina.api.annotation.Endpoint;
-import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.annotation.*;
 import com.zebrunner.carina.api.http.HttpMethodType;
 import com.zebrunner.carina.api.http.HttpResponseStatusType;
-import io.restassured.response.Response;
 
 @Endpoint(url = "${base_url}/search/photos", methodType = HttpMethodType.GET)
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class UnsplashAPIGetPhotos extends UnsplashAPI {
-
-    public UnsplashAPIGetPhotos() {
+    public UnsplashAPIGetPhotos(String query, int perPage) {
         super();
-    }
-
-    public Response getPhotos(String query, int perPage) {
         addUrlParameter("query", query);
         addUrlParameter("per-page", String.valueOf(perPage));
         setHeader("Authorization", "Client-ID " + API_KEY);
-        return callAPIExpectSuccess();
     }
 }
--- src/main/java/com/solvd/carina/demo/api/unsplash/UnsplashAPIGetTopics.java
+++ src/main/java/com/solvd/carina/demo/api/unsplash/UnsplashAPIGetTopics.java
@@ -3,18 +3,14 @@ import com.zebrunner.carina.api.annotation.Endpoint;
 import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
 import com.zebrunner.carina.api.http.HttpMethodType;
 import com.zebrunner.carina.api.http.HttpResponseStatusType;
-import io.restassured.response.Response;
 
 @Endpoint(url = "${base_url}/topics", methodType = HttpMethodType.GET)
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class UnsplashAPIGetTopics extends UnsplashAPI {
-    public UnsplashAPIGetTopics() {
+    public UnsplashAPIGetTopics(int pageCount) {
         super();
-    }
-
-    public Response getTopics(int pageCount) {
         addUrlParameter("page", String.valueOf(pageCount));
         setHeader("Authorization", "Client-ID " + API_KEY);
-        return callAPIExpectSuccess();
     }
+
 }
