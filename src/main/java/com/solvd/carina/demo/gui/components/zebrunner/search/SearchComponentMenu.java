f5602361e901b001dd6affcc64bc21c0dbff6385
JishaG <jishar2023@gmail.com>
2023-08-03 12:44:02 -0400

    Updated searchComponentTest

--- src/main/java/com/solvd/carina/demo/gui/components/zebrunner/search/SearchComponentMenu.java
+++ src/main/java/com/solvd/carina/demo/gui/components/zebrunner/search/SearchComponentMenu.java
@@ -6,11 +6,8 @@ import com.zebrunner.carina.webdriver.locator.Context;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SearchComponentMenu extends AbstractUIObject {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SearchComponentMenu.class);
     @FindBy(xpath="//form[@class='md-search__form']")
     private ExtendedWebElement searchComponentOnHeader;
     @Context(dependsOn = "searchComponentOnHeader")
@@ -19,30 +16,22 @@ public class SearchComponentMenu extends AbstractUIObject {
     @Context(dependsOn = "searchComponentOnHeader")
     @FindBy(tagName = "input")
     private ExtendedWebElement searchInputForm;
+    @FindBy(xpath="//input[@placeholder='Search']")
+    private ExtendedWebElement searchInputFormText;
 
     public SearchComponentMenu(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
     }
 
-    public boolean isLogoAndInputFormWithPlaceholderPresentInSearchComponent() {
-        LOGGER.info("Attempting to get search Icon");
-        boolean isSearchIconPresent = searchIcon.isPresent();
-        LOGGER.info("Attempting to get search Input form");
-        boolean isSearchInputFormPresent = searchInputForm.isPresent();
-        boolean isSearchIconAndInputFormPresent = isSearchIconPresent && isSearchInputFormPresent;
-        if (isSearchIconAndInputFormPresent) {
-            LOGGER.info("Attempting to get the placeholder text in input form");
-            String inputFormPlaceHolder = searchInputForm.getAttribute("placeholder");
-            return inputFormPlaceHolder.equals("Search");
-        }
-        return false;
+    public boolean isSearchComponentMadeOfSearchIconAndInputForm() {
+        boolean isSearchIconPresent = searchIcon.isElementPresent();
+        boolean isSearchInputFormPresent = searchInputForm.isElementPresent();
+        boolean isPlaceholderTextPresentInInputForm = searchInputFormText.isElementPresent();
+        return isSearchIconPresent && isSearchInputFormPresent && isPlaceholderTextPresentInInputForm;
     }
 
-    public boolean isSearchComponentMadeOfSearchIconAndInputForm() {
-        LOGGER.info("Search component is made of icon and input with ‘Search’ text");
-        ExtendedWebElement searchIconChild = searchComponentOnHeader.findExtendedWebElement(searchIcon.getBy());
-        ExtendedWebElement inputFormChild = searchComponentOnHeader.findExtendedWebElement(searchInputForm.getBy());
-        return searchIconChild.isVisible() && inputFormChild.isVisible();
+    public String getSearchInputFormHasPlaceholderAttribute() {
+        return searchInputFormText.getAttribute("placeholder");
     }
 
 }
